254136a4181a46c12788962bd0e86187f954739f
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2023-08-28 11:20:59 -0600

    lab

---
+++ code/part4/lab/calculatorexample/src/sample/Calculator.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Calculator {
+  public int add(int op1, int op2) {
+    return op1 + op2;
+  }
+
+  public int divide(int numerator, int denominator) {
+    return numerator / denominator;
+  }
+}
